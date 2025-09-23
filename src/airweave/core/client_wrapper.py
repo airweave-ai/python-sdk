9e8469bd1c91b9012e166471384305e226176af9
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-09-23 18:57:59 +0000

    Release v0.6.12

--- src/airweave/core/client_wrapper.py
+++ src/airweave/core/client_wrapper.py
@@ -22,10 +22,10 @@ class BaseClientWrapper:
 
     def get_headers(self) -> typing.Dict[str, str]:
         headers: typing.Dict[str, str] = {
-            "User-Agent": "airweave-sdk/v0.6.11",
+            "User-Agent": "airweave-sdk/v0.6.12",
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "airweave-sdk",
-            "X-Fern-SDK-Version": "v0.6.11",
+            "X-Fern-SDK-Version": "v0.6.12",
             **(self.get_custom_headers() or {}),
         }
         headers["x-api-key"] = self.api_key
